eae5a2d53848bf108594f75d239644726a33b41e
Sree Mughi <164155906+sreeC-26@users.noreply.github.com>
2024-07-25 20:01:52 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 /* script.js */
+// typewriter effect functionality
+function typeWriter(element, text, i, callback) {
+    if (i < text.length) {
+        element.innerHTML = text.substring(0, i + 1) + '<span aria-hidden="true"></span>';
+        setTimeout(function() {
+            typeWriter(element, text, i + 1, callback);
+        }, 100);
+    } else if (typeof callback === 'function') {
+        setTimeout(callback, 700);
+    }
+}
+
+// Initiate the typewriter effect
+document.addEventListener('DOMContentLoaded', function() {
+    const introTextElement = document.getElementById('intro-text');
+    const introText = "Hi, I'm Sree Mughi Chandrasekar";
+    typeWriter(introTextElement, introText, 0);
+});
 
 document.querySelectorAll('nav a').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
